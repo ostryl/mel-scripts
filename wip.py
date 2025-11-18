d2c857dab153519859421a0dc2ca8eea5492ece6
Ostryl <ostryl@proton.me>
2025-11-17 19:40:27 -0800

    Added script

--- wip.py
+++ wip.py
@@ -1,7 +1,10 @@
 import maya.cmds as cmds
-import random as cmds
 
-#comment
-print("wake up maya") ;
+#comments look like this
+cmds.select(all=True)
+cmds.polyCube( w=1, h=1, d=1, sx=1, sy=1, sz=1, ax=[0,1,0], cuv=4, ch=True)
 
-string $myShader
+myShader = cmds.shadingNode('anisotropic', aaShader=True)
+
+cmds.select(myCube)
+cmds.hyperShade( assign=myShader )
