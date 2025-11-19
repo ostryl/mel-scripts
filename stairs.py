2317869d4d90a1f1369a28c8448f0e2ea646ee46
Ostryl <ostryl@proton.me>
2025-11-18 22:13:11 -0800

    Added comment and improved stairs.

--- stairs.py
+++ stairs.py
@@ -1,15 +1,33 @@
 import maya.cmds as cmds
 import math
-import random
+import sys
 #I want to make code that generates stairs based on the location of two locators, which the user can control in the viewport
 #The user should be able to change the height/size of the stairs and the code should accomodate that.
-#How is this better than MASH, though? Seems like a waste of time but meh, screw it
 #Would be cool if it had a GUI
-#Clear selection
-cmds.select (clear=1)
-#Create locators
+
+#---This section gets the number of selected objects, and throws an error if more or less than one object is selected.
+SelectedObj = cmds.ls(selection=True)
+Number= len(SelectedObj)
+
+if Number == 0:
+    sys.exit("You need to select an object!")
+elif Number > 1:
+    sys.exit("You have multiple objects selected, please only select one!")
+else:
+    print("Stair building in progress...")
+
+#Now, we need a way to use the selected object as the stair.
+
+
+#---This part creates the locators!
 LocatorA = cmds.spaceLocator (name="LocatorA")
 LocatorB = cmds.spaceLocator (name="LocatorB")
+
+#These pieces are temporary. I need to replace StairWidth with the width of the selected object, and cmds.move will need to be based on the locator instead.
+StairWidth = 0.2
+cmds.select(LocatorA)
+cmds.move(5,5,5)
+
 #Maybe I could iterate down the list of locators until the largest two numbers (eg. locator8 and locator9) and select those?
 #There's a name flag. Wow. RTFM, right? How do I get the script to pull custom geometry? 
 #Seriously, is this even worth it when MASH exists?
@@ -22,10 +40,11 @@ LocatorATranslateY = cmds.getAttr ('LocatorA.translateY')
 LocatorBTranslateX = cmds.getAttr ('LocatorB.translateX')
 LocatorBTranslateZ = cmds.getAttr ('LocatorB.translateZ')
 LocatorBTranslateY = cmds.getAttr ('LocatorB.translateY')
-StairWidth = 0.5
+
+
 
 StairNumber = round(((math.sqrt(((pow((LocatorBTranslateZ-LocatorATranslateZ), 2))+(pow((LocatorBTranslateX-LocatorATranslateX), 2)))))/StairWidth))
-print(StairNumber)
+
 Stairs = range(StairNumber)
 #If LocatorATranslateX > LocatorBTranslateX, StairTranslateX = LocatorATranslateX - (Stair width * the # current number of stairs)
 #Elseif LocatorStairTranslateX < LocatorTranslateX, StairTranslateX = LocatorATranslateX + (Stair width * the # current number of stairs)
@@ -33,12 +52,10 @@ Stairs = range(StairNumber)
 
 for stair in Stairs:
     #Define vars
-    print("for")
     #Create geo
+    Stair = cmds.polyCube( w=3, h=1, d=3, sx=2, sy=1, sz=2, ax=[0,1,0], cuv=1, ch=True)
 
 #Testing
-cmds.select(LocatorA)
-cmds.move(5,5,5)
 
 #StairRotation
 #StairRangeX = The difference between LocatorATranslateX and LocatorBTranslateX
