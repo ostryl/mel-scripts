ded83b9b61eb98da9ba40b0652eca2cd979ecc3d
Ostryl <ostryl@proton.me>
2025-11-21 09:24:31 -0800

    Changes

--- stairs.py
+++ stairs.py
@@ -1,36 +1,30 @@
 import maya.cmds as cmds
 import math
 import sys
-#I want to make code that generates stairs based on the location of two locators, which the user can control in the viewport
-#The user should be able to change the height/size of the stairs and the code should accomodate that.
-#Would be cool if it had a GUI
+#--I want to make code that generates stairs based on the location of two locators, which the user can control in the viewport
+#--The user should be able to change the height/size of the stairs and the code should accomodate that.
 
 #---This section gets the number of selected objects, and throws an error if more or less than one object is selected.
 SelectedObj = cmds.ls(selection=True)
 Number= len(SelectedObj)
 
 if Number == 0:
-    sys.exit("You need to select an object!")
+    sys.exit('You need to select an object!')
 elif Number > 1:
-    sys.exit("You have multiple objects selected, please only select one!")
+    sys.exit('You have multiple objects selected, please only select one!')
 else:
-    print("Stair building in progress...")
+    print('Stair building in progress...')
+    cmds.rename (SelectedObj, 'StairStep')
 
-#Now, we need a way to use the selected object as the stair.
+#---This part creates the locators.
+LocatorA = cmds.spaceLocator (name='LocatorA')
+LocatorB = cmds.spaceLocator (name='LocatorB')
 
-
-#---This part creates the locators!
-LocatorA = cmds.spaceLocator (name="LocatorA")
-LocatorB = cmds.spaceLocator (name="LocatorB")
-
-#These pieces are temporary. I need to replace StairWidth with the width of the selected object, and cmds.move will need to be based on the locator instead.
-StairWidth = 0.2
+#These pieces are temporary and should be removed. I need to replace StairWidth with the width of the selected object, and cmds.move will need to be based on the locator instead.
+StairWidth = 1
 cmds.select(LocatorA)
 cmds.move(5,5,5)
 
-#Maybe I could iterate down the list of locators until the largest two numbers (eg. locator8 and locator9) and select those?
-#There's a name flag. Wow. RTFM, right? How do I get the script to pull custom geometry? 
-#Seriously, is this even worth it when MASH exists?
 #The position X of the stairs should be the width of the distance between. The stairs should rotate to face the direction of the lower locator
 #There should be a way to add some random tilting to make the stairs look old.
 
@@ -41,20 +35,24 @@ LocatorBTranslateX = cmds.getAttr ('LocatorB.translateX')
 LocatorBTranslateZ = cmds.getAttr ('LocatorB.translateZ')
 LocatorBTranslateY = cmds.getAttr ('LocatorB.translateY')
 
-
-
+#--This part gets the length of the space between LocatorA and LocatorB, and then divides it by the width of the stairs-
+#and rounds it to the nearest integer. This determines the number of stairs that will be generated.
 StairNumber = round(((math.sqrt(((pow((LocatorBTranslateZ-LocatorATranslateZ), 2))+(pow((LocatorBTranslateX-LocatorATranslateX), 2)))))/StairWidth))
 
 Stairs = range(StairNumber)
 #If LocatorATranslateX > LocatorBTranslateX, StairTranslateX = LocatorATranslateX - (Stair width * the # current number of stairs)
 #Elseif LocatorStairTranslateX < LocatorTranslateX, StairTranslateX = LocatorATranslateX + (Stair width * the # current number of stairs)
 #Else 
-
+cmds.select (clear=True)
 for stair in Stairs:
     #Define vars
-    #Create geo
-    Stair = cmds.polyCube( w=3, h=1, d=3, sx=2, sy=1, sz=2, ax=[0,1,0], cuv=1, ch=True)
-
+    #--Create geo. We need a way to use the selected object as the stair.
+    cmds.duplicate('StairStep')
+    StairIteration = str(stair+1)
+    cmds.select('StairStep' + StairIteration)
+    cmds.move(stair * StairWidth, stair * StairWidth, stair * StairWidth)
+    cmds.select (clear=True)
+    print(stair)
 #Testing
 
 #StairRotation
